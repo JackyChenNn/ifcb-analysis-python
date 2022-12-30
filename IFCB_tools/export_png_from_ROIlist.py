d291879a5e62281e81a49e5efc3c451a9314c691
JackyChenNn <n63388528@outlook.com>
2022-12-30 14:32:06 +0800

    refactor: Fix the problem that the file cannot be read normally

--- IFCB_tools/export_png_from_ROIlist.py
+++ IFCB_tools/export_png_from_ROIlist.py
@@ -21,18 +21,18 @@ def export_png_from_ROIlist(ROIfile_withpath, outputpath, ROInumbers=None):
     adcfile_open.close()
 
     if filename.startswith('I'):
-        x = adcdata[11]
-        y = adcdata[12]
-        startbyte = adcdata[13]
+        x = adcdata[12]
+        y = adcdata[13]
+        startbyte = adcdata[14]
     else:
         x = adcdata[15]
         y = adcdata[16]
-        startbyte = adcdata[18]
-
-    x = list(map(float, x.split(',')))
-    y = list(map(float, y.split(',')))
-    startbyte = list(map(float, startbyte.split(',')))
-
+        startbyte = adcdata[17]
+        
+    x = list(map(float, x.decode().strip().split(',')))
+    y = list(map(float, y.decode().strip().split(',')))
+    startbyte = list(map(float, startbyte.decode().strip().split(',')))
+        
     if ROInumbers is None:
         ROInumbers = [i for (i, v) in enumerate(x) if v > 0]
 
@@ -41,11 +41,13 @@ def export_png_from_ROIlist(ROIfile_withpath, outputpath, ROInumbers=None):
     for count in range(len(ROInumbers)):
         num = ROInumbers[count]
         fid.seek(int(startbyte[num]), 0)
-        img = fid.read(int(x[num] * y[num]))
+        img = fid.read(int(x[num]) * int(y[num]))
+        
         img = np.array([x for x in img])
-        img = np.transpose(img)
+        img = img.reshape(int(x[num]), int(y[num]))
+        print(num)
         pngname = filename + "_" + eng.num2str(num, '%05.0f') + '.png'
-        if len(img) > 0:
-            eng.imwrite(np.uint8('img'), os.path.join(outputpath, pngname))
-
+        if img.size > 0:
+            cv2.imwrite(os.path.join(outputpath, pngname), img.astype(int))
+            
     fid.close()
