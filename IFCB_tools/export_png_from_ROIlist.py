af5b6f51904e03de650f2fd2321eb9c3cf4d15a2
JackyChenNn <n63388528@outlook.com>
2023-03-31 22:23:57 +0800

    fix: fix the bug before, finish the 'export_png_from_ROIlist.py' file conversion.

--- IFCB_tools/export_png_from_ROIlist.py
+++ IFCB_tools/export_png_from_ROIlist.py
@@ -1,53 +1,42 @@
 import os
 import numpy as np
-import matlab
-import matlab.engine
-
+from PIL import Image
 
 def export_png_from_ROIlist(ROIfile_withpath, outputpath, ROInumbers=None):
-    ROIfile_withpath = "/Users/chenhaodong/Downloads/FYP/IFCBdata/D20200216T000215_IFCB999"
-    outputpath = '/Users/chenhaodong/Downloads/FYP/IFCBdata/output'
-    ROInumbers = None
-    (basedir, filename) = os.path.split(
-        ROIfile_withpath)  # extract the base directory and the filename with the given path
-    (file, ext) = os.path.splitext(ROIfile_withpath)  # extract the extension with the given path
-
+    
+    (basedir, filename) = os.path.split(ROIfile_withpath)
+    (file, ext) = os.path.splitext(ROIfile_withpath)
+    
     if not os.path.exists(outputpath):
         os.mkdir(outputpath)
-
-    adcfile = filename + '.adc'
-    adcfile_open = open(os.path.join(basedir, adcfile), "r")
-    adcdata = adcfile_open.readlines()
-    adcfile_open.close()
-
-    if filename.startswith('I'):
-        x = adcdata[12]
-        y = adcdata[13]
-        startbyte = adcdata[14]
-    else:
-        x = adcdata[15]
-        y = adcdata[16]
-        startbyte = adcdata[17]
         
-    x = list(map(float, x.decode().strip().split(',')))
-    y = list(map(float, y.decode().strip().split(',')))
-    startbyte = list(map(float, startbyte.decode().strip().split(',')))
+    adcfile = filename + '.adc'
+    adcdata = np.genfromtxt(adcfile, delimiter=',', dtype=float)
+        
+    if filename.startswith('I'):
+        x = adcdata[:, 12]
+        y = adcdata[:, 13]
+        startbyte = adcdata[:, 14]
+    else:
+        x = adcdata[:, 15]
+        y = adcdata[:, 16]
+        startbyte = adcdata[:, 17]
         
     if ROInumbers is None:
-        ROInumbers = [i for (i, v) in enumerate(x) if v > 0]
+        ROInumbers = np.where(x > 0)[0] + 1
 
-    fid = open(ROIfile_withpath + '.roi', "rb+")
-    eng = matlab.engine.start_matlab()
-    for count in range(len(ROInumbers)):
-        num = ROInumbers[count]
-        fid.seek(int(startbyte[num]), 0)
-        img = fid.read(int(x[num]) * int(y[num]))
-        
-        img = np.array([x for x in img])
-        img = img.reshape(int(x[num]), int(y[num]))
-        print(num)
-        pngname = filename + "_" + eng.num2str(num, '%05.0f') + '.png'
-        if img.size > 0:
-            cv2.imwrite(os.path.join(outputpath, pngname), img.astype(int))
-            
+    with open(ROIfile_withpath + '.roi', 'rb') as fid:
+        for count in range(len(ROInumbers)):
+            num = ROInumbers[count]
+            fid.seek(int(startbyte[num-1]), 0)
+            img = np.fromfile(fid, dtype=np.uint8, count=int(x[num-1]*y[num-1]))
+            img = img.reshape(int(x[num-1]), int(y[num-1]), order='F')
+
+            if img.size > 0:
+                img = np.uint8(img)
+                pngname = f"{filename}_{num:05}.png"
+                outputfile = os.path.join(outputpath, pngname)
+                im = Image.fromarray(img)
+                im.save(outputfile)
+
     fid.close()
